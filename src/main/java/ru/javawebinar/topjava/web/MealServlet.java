e4dfa13cee6ddfc33811749dc01a34da7274aa52
Alex_M <leshka18@gmail.com>
2018-10-08 16:00:15 +0300

    HW01_Done

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,23 +1,38 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.dao.DataInMemory;
+import ru.javawebinar.topjava.model.MealWithExceed;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class MealServlet extends HttpServlet {
-    private static final Logger log = getLogger(UserServlet.class);
+    private static final Logger log = getLogger(MealServlet.class);
+    private static final LocalTime startTime = LocalTime.MIN;
+    private static final LocalTime endTime = LocalTime.MAX;
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    private DataInMemory data = new DataInMemory();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("redirect to meals");
 
-//        request.getRequestDispatcher("/meals.jsp").forward(request, response);
-        response.sendRedirect("meals.jsp");
+        List<MealWithExceed> mealList = MealsUtil.getFilteredWithExceededInOnePass(data.getMealList(), startTime, endTime, 2000);
+
+        request.setAttribute("mealList", mealList);
+        request.setAttribute("formatter", formatter);
+
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
+//        response.sendRedirect("meals.jsp");
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,10 +1,33 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <title>Users</title>
+
 </head>
 <body>
 <h3><a href="index.html">Home</a></h3>
 <h2>Meals</h2>
+
+<c:if test="${!empty mealList}">
+    <table border="1">
+        <thead>
+        <tr>
+            <th>Date/Time</th>
+            <th>Description</th>
+            <th>Calories</th>
+        </tr>
+        </thead>
+        <tbody>
+        <c:forEach items="${mealList}" var="meal">
+            <tr style="${meal.exceed ? 'color:red' : 'color:green'}">
+                <td>${formatter.format(meal.dateTime)}</td>
+                <td>${meal.description}</td>
+                <td>${meal.calories}</td>
+            </tr>
+        </c:forEach>
+        </tbody>
+    </table>
+</c:if>
 </body>
 </html>
